355bc5941372296652799b6be55894fe997458ce
rly907 <rly907@gmail.com>
2024-11-25 12:45:07 -0800

    code rewrite
    rewritten the code base

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
 #include "main.h"
 
+int speed = 1;
+
 /**
  * A callback function for LLEMU's center button.
  *
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
+void on_left_button() {
+	speed = speed - 0.25;
+	pros::lcd::print(0, "Speed down");
+}
+
+void on_right_button() {
+	speed = speed + 0.25;
+	pros::lcd::print(0, "Speed up");
 }
 
 /**
@@ -24,9 +26,11 @@ void on_center_button() {
  */
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::print(0, "Init Done!");
+
+	pros::lcd::register_btn0_cb(on_left_button);
+	pros::lcd::register_btn2_cb(on_right_button);
 }
 
 /**
@@ -75,20 +79,18 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::MotorGroup left_mg({10 , 20});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({1 , 11});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
+	
 
 		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
+		int left = speed * master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
+		int right = -1 * speed * master.get_analog(ANALOG_RIGHT_Y);  // Gets the turn left/right from right joystick
+		left_mg.move(left);                      // Sets left motor voltage
+		right_mg.move(right);                     // Sets right motor voltage
 		pros::delay(20);                               // Run for 20 ms then update
 	}
 }
